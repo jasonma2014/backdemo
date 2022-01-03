414655a10925259219d7d505dbb915ad845c17a3
Jason Ma <majian2014@foxmail.com>
2022-01-03 23:09:25 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import backtrader as bt
+
+if __name__ == "__main__":
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000.0)
+
+    print("Starting portfolio value: %.2f" % cerebro.broker.get_value())
+    cerebro.run()
+    print("final Portfolio value: %.2f" % cerebro.broker.get_value())
+
+
